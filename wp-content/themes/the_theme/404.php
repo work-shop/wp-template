062b8bf4dd5865baa08c784b9a050ca24f7cceef
Greg Nemes <mail@gregnemes.com>
2014-05-28 17:30:57 -0400

    Initial changes to template theme
    stripping unnecessary or specific functionality
    renamed them “the_theme”

---
+++ wp-content/themes/the_theme/404.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+
+	<div class="container page page-404">
+		<div class="row">
+			
+			<div class="col-sm-8 col-sm-offset-2 brand">	
+				<h2 class="">
+					Oh No! Looks like the page you're looking for isn't here anymore. Sorry about that! Try heading back to our 
+					<a href="<?php bloginfo('site_url'); ?>">Home Page.</a>
+				</h2>
+			</div>	
+		
+		</div>		
+	</div>	
+
+<?php get_footer();?>
+
